c0e0e0aa903dead2fbd0e8a521f97e429bbd489f
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-21 23:42:49 +0900

    データをdata.jsに格納

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id: 1,
+    title: "Samsung Galaxy S8",
+    price: `$399.99`,
+    img: `${process.env.PUBLIC_URL}/img/yedjpkwxljtb75t3tezl.png`,
+    alt: "Samsung Galaxy S8",
+  },
+  {
+    id: 2,
+    title: "Google Pixel",
+    price: `$499.99`,
+    img: `${process.env.PUBLIC_URL}/img/phone-1_gvesln.png`,
+    alt: "Google Pixel",
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: `$699.99`,
+    img: `${process.env.PUBLIC_URL}/img/phone-3_h2s6fo.png`,
+    alt: "Xiaomi Redmi Note 2",
+  },
+  {
+    id: 4,
+    title: "Samsung Galaxy S7",
+    price: `$599.99`,
+    img: `${process.env.PUBLIC_URL}/img/phone-2_ohtt5s.png`,
+    alt: "Samsung Galaxy S7",
+  },
+];

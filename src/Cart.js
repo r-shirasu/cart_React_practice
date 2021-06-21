5c6ea3ddbef02d30c754bd1fb3a8013b81231058
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-21 23:57:30 +0900

    imgサイズなど調整

--- src/Cart.js
+++ src/Cart.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Cart.scss";
+import { Data } from "./Data";
 
 export const Cart = () => {
-  return <div>Hello!</div>;
+  return (
+    <article className="cartItem">
+      <img src={Data[0].img} alt={Data[0].alt}></img>
+      <div className="item-info">
+        <h4 className="title">{Data[0].title}</h4>
+        <h4 className="price">{Data[0].price}</h4>
+      </div>
+    </article>
+  );
 };
